824820688d23ce516be3b9f6a718aca3c9c71dd7
chetan saini <nokia1234>
2015-07-28 01:20:24 -0500

    UI setupt done, UI init done in seperate method called initUI()

--- EightPuzzle/src/com/example/eightpuzzle/MainActivity.java
+++ EightPuzzle/src/com/example/eightpuzzle/MainActivity.java
@@ -1,19 +1,77 @@
 package com.example.eightpuzzle;
 
+
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.provider.Settings.Global;
+import android.util.DisplayMetrics;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.widget.Button;
+import android.widget.GridView;
+import android.widget.TextView;
 
 public class MainActivity extends Activity
 {
+private TextView whatis8puzzle,tv_count;
+private Button button_submitSolveAI,button_nextPuzzle;
+private GridView gridView_PuzzleImage;
+private int countMovesMade=0;
+private Context context;
+private ImageAdapter2 imageAdapter2;
+private int[] puzzle=new int[8];
+public static int gwidth;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		DisplayMetrics dmetrics = new DisplayMetrics();
+		getWindowManager().getDefaultDisplay().getMetrics(dmetrics);
+		gwidth = dmetrics.widthPixels / 3;
+		initUI();//initialize the UI
+		startGame();
 	}
+/**
+ * Method to start the game;
+ */
+	private void startGame()
+	{
+		
+		
+	}
+	
+	/**
+	 * Initialize the UI elements
+	 */
+	private void initUI()
+	{
+		whatis8puzzle=(TextView)findViewById(R.id.tv_whatis8puzzle);
+		tv_count=(TextView)findViewById(R.id.tv_count);
+		button_submitSolveAI=(Button)findViewById(R.id.button_SolveUsingAI);
+		button_nextPuzzle=(Button)findViewById(R.id.button_NextPuzzle);
+		gridView_PuzzleImage=(GridView)findViewById(R.id.gridView_puzzleImageGrid);
+		
+		Animation anim = new AlphaAnimation(0.0f, 1.0f);
+		anim.setDuration(150); // You can manage the blinking time with this
+		anim.setStartOffset(50);
+		anim.setRepeatMode(Animation.REVERSE);
+		anim.setRepeatCount(Animation.INFINITE);
+		tv_count.setAnimation(anim);
+		tv_count.setText(String.valueOf(countMovesMade));
+		context= getApplicationContext();
+		imageAdapter2 = new ImageAdapter2(context);
+		imageAdapter2.setPuzzle(StaticVariableHolder.puzzle);// initialize the puzzle
+		imageAdapter2.setImages();
+		gridView_PuzzleImage.setAdapter(imageAdapter2);
+		
+	}
+	
+	
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu)
---
+++ EightPuzzle/src/com/example/eightpuzzle/StaticVariableHolder.java
@@ -0,0 +1,18 @@
+
+
+package com.example.eightpuzzle;
+
+
+public class StaticVariableHolder
+{
+
+    public static int MOVE_DOWN = 4;
+    public static int MOVE_LEFT = 1;
+    public static int MOVE_RIGHT = 2;
+    public static int MOVE_UP = 3;
+    public static String fileName = "EightPuzzleData";
+    public static String solutionSteps = "";
+    public static boolean usingAI = false;
+    public static int[] puzzle={1,3,4,8,6,2,7,0,5};//working
+
+}
